1e0066621cd9a07a8b78f8a8739d099689ba92a6
Thomas <ThomasAntiSoloist@yeah.net>
2019-11-30 09:19:19 +0800

    Fixed the order of rollup's plugins; Improve the mocha behavior;

--- constructor/config.js
+++ constructor/config.js
@@ -22,8 +22,8 @@ const output = [{
 ]
 
 const plugins = [
-    strip(),
-    babel()
+    babel(),
+    strip()
 ]
 
 export default {
--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,10 @@
 import assert from "assert";
-import qiauto from "@qiqi1996/qi-auto";
-import common from "../common";
 
 describe("qi-auto-plugin-template", function () {
     it("should return 'qi-auto' as result", function () {
         let auto = new qiauto({
             "task1": {
-                module: require("../dist/qi-auto-plugin-template.js"),
+                module: require("../dist/index.js"),
                 directory: common.path.EXAMPLE
             }
         });
